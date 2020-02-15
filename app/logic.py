36760cbd1e7099d6405c52c669d931c48dc6180d
Joshua <joshuamblanch@gmail.com>
2020-02-14 21:29:26 -0800

    Going back to previous commit
    again testing

--- app/logic.py
+++ app/logic.py
@@ -1,5 +1,6 @@
 
 
+
 class Point:
     def __init__(self, data=None, x=0, y=0):
         if data != None:
@@ -29,8 +30,9 @@ class GameBoard():
     7 - food
     """
     SnakeBodyCount  = 0 
-    MyBodyCount     = 0
-    DidIJustEat     = False #check if I am about to grow, to omit the tail as a valid square (because I'm growing)
+    MyBodyCount     = 0 
+    DidIJustEat     = False #check if I am about to grow, to omit the tail as a valid square (because I'm growing) #broken
+    Storage_dict    = {} # Stores the health of an individual snake
 
     def __init__(self, data=None):
         """Creates a new game board"""
@@ -52,6 +54,7 @@ class GameBoard():
         # go through all the snakes and add them to the board 
         GameBoard.SnakeBodyCount = 0
         temporary_count = 0
+        GameBoard.Storage_dict = {}
         for snake in data["board"]["snakes"]:
 
             if(snake["id"]==data["you"]["id"]):
@@ -62,6 +65,12 @@ class GameBoard():
                 self.board[bodypart["x"]][bodypart["y"]] = 2
 
                 temporary_count+=1
+
+            '''
+            TODO: store each health into a dictionary (call the function called storage)
+            '''
+
+
 
             if(temporary_count>GameBoard.SnakeBodyCount):
                 GameBoard.SnakeBodyCount = temporary_count
@@ -99,6 +108,8 @@ class GameBoard():
                 print(self.board[y][x], end=' ')
 
             print()
+        print(GameBoard.DidIJustEat)
+        print(self.Storage_dict)
 
     def bfs(self, start, num, status=True):
         """
@@ -131,16 +142,24 @@ class GameBoard():
 
             tile_val = self.board[tile.x][tile.y]
 
+            if(isinstance(tile_val,list)):
+                tile_val = tile_val[0]
+
             if str(tile) in visited:
                 continue
 
             visited.add(str(tile))
+
+            if (GameBoard.DidIJustEat) and (tile_val == 6) :
+                GameBoard.DidIJustEat = False
+                continue
 
             if(not(self.safety_protocol(tile,num)) and status):
                 continue
 
             if(self.trap_protocol(tile)):
                 continue
+
 
             if tile_val == num:
                 return self.get_relative_direction(start, tile, pg)
@@ -154,16 +173,14 @@ class GameBoard():
         points = [Point(x=tile.x, y=(tile.y - 1)), Point(x=tile.x, y=(tile.y + 1)), Point(x=(tile.x - 1), y=tile.y), Point(x=(tile.x + 1), y=tile.y)]
 
         valid_tiles = [0,3,6,7]
-        #safety measure to not enqueue the tail if I just ate a food
-        if(GameBoard.DidIJustEat):
-            valid_tiles.remove(6)
-            GameBoard.DidIJustEat = False  # set it back to false for next iterarion 
 
         for point in points:
             if point.x >= self.width or point.x < 0 or point.y >= self.height or point.y < 0: # to check if our value is out of bounds
                 continue # if it is out of bounds, the iteration is skipped
-            val = self.board[point.x][point.y] 
-            if val in valid_tiles: #queue is only filled with 0,3,7 to start with
+            
+            tile_val = self.board[point.x][point.y] 
+
+            if tile_val in valid_tiles: #queue is only filled with 0,3,6,7 to start with
                 queue.append(point)
 
     def enqueue_around_point(self, tile, queue, visted, parent_graph, num):
@@ -185,6 +202,8 @@ class GameBoard():
         if(self.board[temp.x][temp.y]==7):
             GameBoard.DidIJustEat = True
         
+        print(temp)
+
         diff_x = start.x - temp.x
         diff_y = start.y - temp.y
 
@@ -197,10 +216,19 @@ class GameBoard():
         if diff_y == 1:
             return 0
 
+
+    '''
+    TODO: need to say that it is safe to hit a head if I'm beside a snake with a lower health (not necessarily the biggest health)
+    plan:
+        implement other snake's ID or health to their head
+    '''
     # returns false if the tile is dangerous (beside an opponent snake head)
     # return true if the tile is safe
     def safety_protocol(self,tile, num):
         points = [Point(x=tile.x, y=(tile.y - 1)), Point(x=tile.x, y=(tile.y + 1)), Point(x=(tile.x - 1), y=tile.y), Point(x=(tile.x + 1), y=tile.y)]
+
+        if(num==1): # if you are trying to kill then proceed to collide with head
+            return True
 
         if(GameBoard.AmIAlpha()):
             return True
@@ -214,6 +242,9 @@ class GameBoard():
         
         return True
 
+    '''
+    TODO: need to check further squares AND if the tail is not in sight only then will it be a trapped square 
+    '''
     # Returns True if the next tile is a trapped tile 
     # A tile is considered to be trapped if there are no possible moves after
     def trap_protocol(self,tile):
@@ -234,6 +265,15 @@ class GameBoard():
             return True
         return False 
     
+    # Stores the health of an individual snake
+    # health = health of the snake , id = unique id of the snake 
+    # dictionary will be in the form of {id:health}
+    @staticmethod
+    def Storage(id_string,health):
+        GameBoard.Storage_dict[id_string]=health
+
+
+
     # Will Trap a snake in a corner situation 
     @staticmethod
     def TrapKill():
@@ -274,4 +314,6 @@ Games with bugs :   https://play.battlesnake.com/g/393fcb86-fac1-4cad-b3fe-5e651
                     
                     kill_snake bug (dies of starvation)
                     https://play.battlesnake.com/g/9a0e8a9c-8276-4311-b2e4-7c810a21b1ef/
+                    trapped squares
+                    https://play.battlesnake.com/g/1c2762c9-e322-49ac-9975-6510674ffd78/
 '''
